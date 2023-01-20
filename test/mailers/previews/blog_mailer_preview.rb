ce345559fc1318dab6647d7b54b8176260b1da4c
Markus Zucker <markus.zucker@crisis24.com>
2023-01-20 10:46:51 +0100

    Add mailer preview

--- test/mailers/previews/blog_mailer_preview.rb
+++ test/mailers/previews/blog_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/blog_mailer
 class BlogMailerPreview < ActionMailer::Preview
-
+  def blog_post_published
+    post = BlogPost.create!(content: 'post content')
+    BlogMailer.with(post: post).post_published
+  end
 end
